e406b25779e02c3d1cded75a28a38bb9dbb66784
CoralPink <03_stylist_triages@icloud.com>
2022-12-13 13:33:33 +0900

    feat: mdbook-footnote
    I wanted to change the appearance, so I modified the code and used it.
    refs: https://github.com/daviddrysdale/mdbook-footnote

---
+++ rs/mdbook-footnote/README.md
@@ -0,0 +1,13 @@
+# mdbook-footnote
+
+A preprocessor for [mdbook](https://github.com/rust-lang/mdBook) to support the inclusion of footnotes.
+
+## License and copyright notice
+
+https://github.com/daviddrysdale/mdbook-footnote
+
+This software includes the work that is distributed in the Apache License 2.0.
+
+## State changes
+
+Changed appearance and style.
---
+++ rs/mdbook-footnote/src/main.rs
@@ -0,0 +1,113 @@
+use clap::{App, Arg, ArgMatches, SubCommand};
+use lazy_static::lazy_static;
+use mdbook::{
+    book::Book,
+    errors::Error,
+    preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext},
+};
+use regex::Regex;
+use std::{io, process};
+
+pub fn make_app() -> App<'static> {
+    App::new("footnote-preprocessor")
+        .about("An mdbook preprocessor which converts expands footnote markers")
+        .subcommand(
+            SubCommand::with_name("supports")
+                .arg(Arg::with_name("renderer").required(true))
+                .about("Check whether a renderer is supported by this preprocessor"),
+        )
+}
+
+fn main() {
+    let matches = make_app().get_matches();
+    let preprocessor = Footnote::default();
+
+    if let Some(sub_args) = matches.subcommand_matches("supports") {
+        handle_supports(&preprocessor, sub_args);
+    } else if let Err(e) = handle_preprocessing(&preprocessor) {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
+
+fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
+    let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+
+    if ctx.mdbook_version != mdbook::MDBOOK_VERSION {
+        // We should probably use the `semver` crate to check compatibility
+        // here...
+        eprintln!(
+            "Warning: The {} plugin was built against version {} of mdbook, \
+             but we're being called from version {}",
+            pre.name(),
+            mdbook::MDBOOK_VERSION,
+            ctx.mdbook_version
+        );
+    }
+
+    let processed_book = pre.run(&ctx, book)?;
+    serde_json::to_writer(io::stdout(), &processed_book)?;
+
+    Ok(())
+}
+
+fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
+    let renderer = sub_args.value_of("renderer").expect("Required argument");
+    let supported = pre.supports_renderer(renderer);
+
+    // Signal whether the renderer is supported by exiting with 1 or 0.
+    if supported {
+        process::exit(0);
+    } else {
+        process::exit(1);
+    }
+}
+
+lazy_static! {
+    static ref FOOTNOTE_RE: Regex =
+        Regex::new(r"(?s)\{\{footnote:\s*(?P<content>.*?)\}\}").unwrap();
+}
+
+/// A pre-processor that expands {{footnote: ..}} markers.
+#[derive(Default)]
+pub struct Footnote {}
+
+impl Preprocessor for Footnote {
+    fn name(&self) -> &str {
+        "footnote-preprocessor"
+    }
+
+    fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
+        book.for_each_mut(|item| {
+            if let mdbook::book::BookItem::Chapter(chap) = item {
+                let mut footnotes = vec![];
+
+                chap.content = FOOTNOTE_RE
+                    .replace_all(&chap.content, |caps: &regex::Captures| {
+                        let content = caps.name("content").unwrap().as_str().to_owned();
+                        footnotes.push(content);
+                        let idx = footnotes.len();
+
+                        format!("<sup class=\"footnote-reference\"><a name=\"to-footnote-{}\">[{}](#{})</a></sup>", idx, idx, idx)
+                    })
+                    .to_string();
+
+                if !footnotes.is_empty() {
+                    for (idx, content) in footnotes.into_iter().enumerate() {
+                        let num = idx + 1;
+                        chap.content += &format!("<div class=\"footnote-definition\" id={}>\n", num);
+                        chap.content += &format!("\n\n[<sup>{}</sup>](#to-footnote-{})", num, num);
+                        chap.content += &format!(" {}", content);
+                        chap.content += &format!("\n\n[⤴️](#to-footnote-{})", num);
+                        chap.content += "</div>";
+                    }
+                }
+            }
+        });
+        Ok(book)
+    }
+
+    fn supports_renderer(&self, renderer: &str) -> bool {
+        renderer != "not-supported"
+    }
+}
