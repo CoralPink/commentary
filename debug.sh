8542ca68a8256dffbbcd89dec1e310624418c5ee
CoralPink <03_stylist_triages@icloud.com>
2022-12-22 14:17:19 +0900

    Compress book.js searcher.js

--- debug.sh
+++ debug.sh
@@ -6,5 +6,7 @@ npm run debug --prefix scss style.scss ../css/style.css
 
 mkdir js/dist
 npm run uglifyjs --prefix js -input='pagetoc.js' -output='dist/pagetoc.js'
+npm run uglifyjs --prefix js -input='book.js' -output='../theme/book.js'
+npm run uglifyjs --prefix js -input='searcher.js' -output='../src/searcher.js'
 
 mdbook build
