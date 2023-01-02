728c8119246cdc624d5d5cb2dd427dd19f13f110
CoralPink <03_stylist_triages@icloud.com>
2023-01-02 09:53:12 +0900

    Remove unnecessary files and codes.

--- debug.sh
+++ debug.sh
@@ -1,6 +1,5 @@
 npm run debug --prefix scss theme/chrome.scss ../theme/css/chrome.css
 npm run debug --prefix scss theme/general.scss ../theme/css/general.css
-npm run debug --prefix scss theme/variables.scss ../theme/css/variables.css
 npm run debug --prefix scss fonts/fonts.scss ../src/fonts/fonts.css
 npm run debug --prefix scss style.scss ../css/style.css
 
@@ -10,3 +9,5 @@ npm run uglifyjs --prefix js -input='book.js' -output='../theme/book.js'
 npm run uglifyjs --prefix js -input='searcher.js' -output='../src/searcher.js'
 
 mdbook build
+
+rm book/css/variables.css
