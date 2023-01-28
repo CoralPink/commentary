ecc303f6d472581f1e0a9359aa5fe7eb249bd729
CoralPink <03_stylist_triages@icloud.com>
2023-01-28 22:09:37 +0900

    Switching `uglifyjs` -> `terser` to try it out🦭
    It doesn't particularly change the behavior, but it might be good for a change!!

--- debug.sh
+++ debug.sh
@@ -3,10 +3,10 @@ npm run debug --prefix scss theme/general.scss ../theme/css/general.css
 npm run debug --prefix scss fonts/fonts.scss ../src/fonts/fonts.css
 npm run debug --prefix scss style.scss ../css/style.css
 
-npm run uglifyjs --prefix js -input='start.js' -output='../src/start.js'
-npm run uglifyjs --prefix js -input='searcher.js' -output='../src/searcher.js'
-npm run uglifyjs --prefix js -input='book.js' -output='../theme/book.js'
+npm run terser --prefix js -input='start.js' -output='../src/start.js'
+npm run terser --prefix js -input='searcher.js' -output='../src/searcher.js'
+npm run terser --prefix js -input='book.js' -output='../theme/book.js'
 mkdir js/dist
-npm run uglifyjs --prefix js -input='pagetoc.js' -output='dist/pagetoc.js'
+npm run terser --prefix js -input='pagetoc.js' -output='dist/pagetoc.js'
 
 mdbook build
