ca1144cb1f38868843dc3cc38f0882ee4dddc935
CoralPink <03_stylist_triages@icloud.com>
2023-01-31 13:32:37 +0900

    Optimization across the board.
    .github:
      Various reviews and adjustments for post-optimization behavior.
    
    book.toml:
      Reviewed and adjusted parameters that we had not paid much attention to.
    
    js:
      If there is an update, dependabot will detect it... I hope.
      Put `clipboard` `mark.js` under control.
    
    theme:
      Organize code. 🐰

--- debug.sh
+++ debug.sh
@@ -1,12 +1,24 @@
+npm run compress --prefix js -input='book.js' -output='../theme/book.js'
+npm run compress --prefix js -input='searcher.js' -output='../src/searcher.js'
+npm run compress --prefix js -input='start.js' -output='../src/start.js'
+mkdir js/dist
+npm run compress --prefix js -input='pagetoc.js' -output='dist/pagetoc.js'
+
+cp js/node_modules/clipboard/dist/clipboard.min.js src
+cp js/node_modules/mark.js/dist/mark.es6.min.js src
+
+npm run debug --prefix scss style.scss ../css/style.css
+npm run debug --prefix scss fonts/fonts.scss ../src/fonts/fonts.css
 npm run debug --prefix scss theme/chrome.scss ../theme/css/chrome.css
 npm run debug --prefix scss theme/general.scss ../theme/css/general.css
-npm run debug --prefix scss fonts/fonts.scss ../src/fonts/fonts.css
-npm run debug --prefix scss style.scss ../css/style.css
-
-npm run terser --prefix js -input='start.js' -output='../src/start.js'
-npm run terser --prefix js -input='searcher.js' -output='../src/searcher.js'
-npm run terser --prefix js -input='book.js' -output='../theme/book.js'
-mkdir js/dist
-npm run terser --prefix js -input='pagetoc.js' -output='dist/pagetoc.js'
 
 mdbook build
+
+#rm book/ayu-highlight.css
+#rm book/highlight.css
+#rm book/highlight.js
+#rm book/tomorrow-night.css
+#rm book/css/variables.css
+#rm book/mark.min.js
+#rm -rf book/FontAwesome
+#echo 'Complete!!'
