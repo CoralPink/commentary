01210210e66900c8b55b10458002abfabed87ca5
CoralPink <03_stylist_triages@icloud.com>
2023-05-09 15:24:46 +0900

    Optimize the code to work with `fzf` by integrating it more into the main body 🧭

--- js/searcher.js
+++ js/searcher.js
@@ -4,6 +4,35 @@ const rootPath = document.getElementById('searcher').dataset.pathtoroot;
 
 window.search = window.search || {};
 
+/**
+ * @see https://github.com/HillLiu/docker-mdbook
+ */
+window.elasticlunr.Index.load = (index) => {
+  const FzF = window.fzf.Fzf;
+  const storeDocs = index.documentStore.docs;
+  const indexArr = Object.keys(storeDocs);
+  const ofzf = new FzF(indexArr, {
+    selector: (item) => {
+      const res = storeDocs[item];
+      res.text = `${res.title}${res.breadcrumbs}${res.body}`;
+      return res.text;
+    },
+  });
+  return {
+    search: (searchterm) => {
+      const entries = ofzf.find(searchterm);
+      return entries.map((data) => {
+        const { item, score } = data;
+        return {
+          doc: storeDocs[item],
+          ref: item,
+          score,
+        };
+      });
+    },
+  };
+};
+
 // Search functionality
 //
 // You can use !hasFocus() to prevent keyhandling in your key
