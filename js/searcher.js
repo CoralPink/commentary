c1d02ea3987ac941298b087b9f0c596008919397
CoralPink <03_stylist_triages@icloud.com>
2023-06-19 16:54:10 +0900

    Let fzf handle the escaping of search strings ⚖️
    I have a feeling that fzf does it without me having to do it (in fact, it seems to be working fine).
    What do you think?

--- js/searcher.js
+++ js/searcher.js
@@ -8,19 +8,19 @@ const main = search => {
   const URL_SEARCH_PARAM = 'search';
   const URL_MARK_PARAM = 'highlight';
 
-  const search_wrap = document.getElementById('search-wrapper');
-  const searchbar = document.getElementById('searchbar');
-  const searchresults = document.getElementById('searchresults');
-  const searchicon = document.getElementById('search-toggle');
+  const ELEMENT_BAR = document.getElementById('searchbar');
+  const ELEMTNT_WRAPPER = document.getElementById('search-wrapper');
+  const ELEMENT_RESULTS = document.getElementById('searchresults');
+  const ELEMENT_ICON = document.getElementById('search-toggle');
 
-  let doc_urls = [];
+  let docUrls = [];
 
-  let results_options = {
+  let resultsOptions = {
     teaser_word_count: 30,
     limit_results: 30,
   };
 
-  let search_options = {
+  let searchOptions = {
     bool: 'AND',
     expand: true,
     fields: {
@@ -30,79 +30,114 @@ const main = search => {
     },
   };
 
-  let teaser_count = 0;
+  const formatResult = (cnt, result, terms) => {
+    const makeTeaser = (body, terms) => {
+      const stemmed_terms = terms.map(w => elasticlunr.stemmer(w.toLowerCase()));
+      const weighted = []; // contains elements of ["word", weight, index_in_document]
+      const WEIGHT = 40;
 
-  const removeChildren = elem => {
-    while (elem.firstChild) {
-      elem.removeChild(elem.firstChild);
-    }
-  };
+      let value = 0;
+      let idx = 0;
+      let found = false;
 
-  // Helper to parse a url into its building blocks.
-  const parseURL = url => {
-    const a = document.createElement('a');
-    a.href = url;
+      body
+        .toLowerCase()
+        .split('. ') // split in sentences, then words
+        .forEach(x => {
+          const words = x.split(' ');
+          value = 8;
 
-    return {
-      source: url,
-      protocol: a.protocol.replace(':', ''),
-      host: a.hostname,
-      port: a.port,
-      params: (() => {
-        const ret = {};
-        const seg = a.search.replace(/^\?/, '').split('&');
-        let i = 0;
+          words.forEach(y => {
+            if (y.length > 0) {
+              stemmed_terms.forEach(z => {
+                if (elasticlunr.stemmer(y).startsWith(z)) {
+                  value = WEIGHT;
+                  found = true;
+                }
+              });
+              weighted.push([y, value, idx]);
+              value = 2;
+            }
+            idx += y.length;
+            idx += 1; // ' ' or '.' if last word in sentence
+          });
 
-        for (; i < seg.length; i++) {
-          if (!seg[i]) {
-            continue;
+          idx += 1; // because we split at a two-char boundary '. '
+        });
+
+      if (weighted.length == 0) {
+        return body;
+      }
+
+      const window_size = Math.min(weighted.length, resultsOptions.teaser_word_count);
+      let cur_sum = 0;
+
+      for (let wordindex = 0; wordindex < window_size; wordindex++) {
+        cur_sum += weighted[wordindex][1];
+      }
+
+      const window_weight = [];
+      window_weight.push(cur_sum);
+
+      for (let wordindex = 0; wordindex < weighted.length - window_size; wordindex++) {
+        cur_sum -= weighted[wordindex][1];
+        cur_sum += weighted[wordindex + window_size][1];
+        window_weight.push(cur_sum);
+      }
+
+      let max_sum_window_index = 0;
+
+      if (found) {
+        let max_sum = 0;
+
+        // backwards
+        for (let i = window_weight.length - 1; i >= 0; i--) {
+          if (window_weight[i] > max_sum) {
+            max_sum = window_weight[i];
+            max_sum_window_index = i;
           }
-          const s = seg[i].split('=');
-          ret[s[0]] = s[1];
         }
-        return ret;
-      })(),
-      file: (a.pathname.match(/\/([^/?#]+)$/i) || [, ''])[1],
-      hash: a.hash.replace('#', ''),
-      path: a.pathname.replace(/^([^/])/, '/$1'),
-    };
-  };
+      } else {
+        max_sum_window_index = 0;
+      }
 
-  // Helper to escape html special chars for displaying the teasers
-  const escapeHTML = (() => {
-    const MAP = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&#34;',
-      "'": '&#39;',
-    };
+      const teaser = [];
+      let index = weighted[max_sum_window_index][2];
 
-    const repl = c => {
-      return MAP[c];
-    };
+      const pushTeaser = word => {
+        index = word[2] + word[0].length;
+        teaser.push(body.substring(word[2], index));
+      };
 
-    return s => {
-      return s.replace(/[&<>'"]/g, repl);
-    };
-  })();
+      for (let i = max_sum_window_index; i < max_sum_window_index + window_size; i++) {
+        const word = weighted[i];
 
-  const formatSearchResult = (result, searchterms) => {
-    const teaser = makeTeaser(escapeHTML(result.doc.body), searchterms);
-    teaser_count++;
+        // missing text from index to start of `word`
+        if (index < word[2]) {
+          teaser.push(body.substring(index, word[2]));
+          index = word[2];
+        }
+
+        if (word[1] != WEIGHT) {
+          pushTeaser(word);
+          continue;
+        }
+        teaser.push('<em>');
+        pushTeaser(word);
+        teaser.push('</em>');
+      }
+      return teaser.join('');
+    };
 
     // The ?URL_MARK_PARAM= parameter belongs inbetween the page and the #heading-anchor
-    const url = doc_urls[result.ref].split('#');
+    const url = docUrls[result.ref].split('#');
 
+    // no anchor found
     if (url.length == 1) {
-      // no anchor found
       url.push('');
     }
 
-    // encodeURIComponent escapes all chars that could allow an XSS except
-    // for '. Due to that we also manually replace ' with its url-encoded
-    // representation (%27).
-    const st = encodeURIComponent(searchterms.join(' ')).replace(/\'/g, '%27');
+    const num = cnt + 1;
 
     return (
       '<a href="' +
@@ -111,161 +146,53 @@ const main = search => {
       '?' +
       URL_MARK_PARAM +
       '=' +
-      st +
+      terms +
       '#' +
       url[1] +
       '" aria-details="teaser_' +
-      teaser_count +
+      num +
       '">' +
       result.doc.breadcrumbs +
       '</a>' +
       '<span class="teaser" id="teaser_' +
-      teaser_count +
+      num +
       '" aria-label="Search Result Teaser">' +
-      teaser +
+      makeTeaser(result.doc.body, terms) +
       '</span>'
     );
   };
 
-  const makeTeaser = (body, searchterms) => {
-    // The strategy is as follows:
-    // First, assign a value to each word in the document:
-    //  Words that correspond to search terms (stemmer aware): 40
-    //  Normal words: 2
-    //  First word in a sentence: 8
-    // Then use a sliding window with a constant number of words and count the
-    // sum of the values of the words within the window. Then use the window that got the
-    // maximum sum. If there are multiple maximas, then get the last one.
-    // Enclose the terms in <em>.
-    const stemmed_searchterms = searchterms.map(function (w) {
-      return elasticlunr.stemmer(w.toLowerCase());
-    });
-    const searchterm_weight = 40;
-
-    let idx = 0;
-    let value = 0;
-    let searchterm_found = false;
-
-    const weighted = []; // contains elements of ["word", weight, index_in_document]
-
-    body
-      .toLowerCase()
-      .split('. ') // split in sentences, then words
-      .forEach(x => {
-        const words = x.split(' ');
-        value = 8;
-
-        words.forEach(y => {
-          if (y.length > 0) {
-            stemmed_searchterms.forEach(z => {
-              if (elasticlunr.stemmer(y).startsWith(z)) {
-                value = searchterm_weight;
-                searchterm_found = true;
-              }
-            });
-            weighted.push([y, value, idx]);
-            value = 2;
-          }
-          idx += y.length;
-          idx += 1; // ' ' or '.' if last word in sentence
-        });
-
-        idx += 1; // because we split at a two-char boundary '. '
-      });
-
-    if (weighted.length == 0) {
-      return body;
-    }
-
-    const window_weight = [];
-    const window_size = Math.min(weighted.length, results_options.teaser_word_count);
-
-    let cur_sum = 0;
-
-    for (let wordindex = 0; wordindex < window_size; wordindex++) {
-      cur_sum += weighted[wordindex][1];
-    }
-    window_weight.push(cur_sum);
-
-    for (let wordindex = 0; wordindex < weighted.length - window_size; wordindex++) {
-      cur_sum -= weighted[wordindex][1];
-      cur_sum += weighted[wordindex + window_size][1];
-      window_weight.push(cur_sum);
-    }
-
-    let max_sum_window_index = 0;
-
-    if (searchterm_found) {
-      let max_sum = 0;
-
-      // backwards
-      for (let i = window_weight.length - 1; i >= 0; i--) {
-        if (window_weight[i] > max_sum) {
-          max_sum = window_weight[i];
-          max_sum_window_index = i;
-        }
-      }
-    } else {
-      max_sum_window_index = 0;
-    }
-
-    // add <em/> around searchterms
-    const teaser_split = [];
-    let index = weighted[max_sum_window_index][2];
-
-    for (let i = max_sum_window_index; i < max_sum_window_index + window_size; i++) {
-      const word = weighted[i];
-
-      if (index < word[2]) {
-        // missing text from index to start of `word`
-        teaser_split.push(body.substring(index, word[2]));
-        index = word[2];
-      }
-      if (word[1] == searchterm_weight) {
-        teaser_split.push('<em>');
-      }
-      index = word[2] + word[0].length;
-      teaser_split.push(body.substring(word[2], index));
-
-      if (word[1] == searchterm_weight) {
-        teaser_split.push('</em>');
-      }
-    }
-    return teaser_split.join('');
-  };
-
   const showSearch = () => {
-    search_wrap.classList.remove('hidden');
-    searchicon.setAttribute('aria-expanded', 'true');
+    ELEMTNT_WRAPPER.classList.remove('hidden');
+    ELEMENT_ICON.setAttribute('aria-expanded', 'true');
   };
 
   const hiddenSearch = () => {
-    search_wrap.classList.add('hidden');
-    searchicon.setAttribute('aria-expanded', 'false');
+    ELEMTNT_WRAPPER.classList.add('hidden');
+    ELEMENT_ICON.setAttribute('aria-expanded', 'false');
 
-    if (searchresults.length == null) {
+    if (ELEMENT_RESULTS.length == null) {
       return;
     }
-
-    searchresults.children.forEach(x => x.classList.remove('focus'));
+    ELEMENT_RESULTS.children.forEach(x => x.classList.remove('focus'));
   };
 
   const init = config => {
     const marker = new Mark(document.querySelector('main'));
     const mark_exclude = [];
 
-    results_options = config.results_options;
-    search_options = config.search_options;
-    doc_urls = config.doc_urls;
+    resultsOptions = config.results_options;
+    searchOptions = config.search_options;
+    docUrls = config.doc_urls;
 
     // Eventhandler for search icon
-    searchicon.addEventListener(
+    ELEMENT_ICON.addEventListener(
       'click',
       () => {
-        if (search_wrap.classList.contains('hidden')) {
+        if (ELEMTNT_WRAPPER.classList.contains('hidden')) {
           showSearch();
           window.scrollTo(0, 0);
-          searchbar.select();
+          ELEMENT_BAR.select();
         } else {
           hiddenSearch();
         }
@@ -273,15 +200,50 @@ const main = search => {
       { once: false, passive: true }
     );
 
+    // Helper to parse a url into its building blocks.
+    const parseURL = url => {
+      const a = document.createElement('a');
+      a.href = url;
+
+      return {
+        source: url,
+        protocol: a.protocol.replace(':', ''),
+        host: a.hostname,
+        port: a.port,
+        params: (() => {
+          const ret = {};
+          const seg = a.search.replace(/^\?/, '').split('&');
+          let i = 0;
+
+          for (; i < seg.length; i++) {
+            if (!seg[i]) {
+              continue;
+            }
+            const s = seg[i].split('=');
+            ret[s[0]] = s[1];
+          }
+          return ret;
+        })(),
+        file: (a.pathname.match(/\/([^/?#]+)$/i) || [, ''])[1],
+        hash: a.hash.replace('#', ''),
+        path: a.pathname.replace(/^([^/])/, '/$1'),
+      };
+    };
+
     // Eventhandler for keyevents while the searchbar is focused
-    const searchbarKeyUpHandler = () => {
+    const keyUpHandler = () => {
+      const removeChildren = elem => {
+        while (elem.firstChild) {
+          elem.removeChild(elem.firstChild);
+        }
+      };
+
       const doSearch = term => {
-        // Clear and insert results
-        removeChildren(searchresults);
+        removeChildren(ELEMENT_RESULTS);
 
         // Do the actual search
-        const results = elasticlunr.Index.load(config.index).search(term, search_options);
-        const count = Math.min(results.length, results_options.limit_results);
+        const results = elasticlunr.Index.load(config.index).search(term, searchOptions);
+        const count = Math.min(results.length, resultsOptions.limit_results);
 
         document.getElementById('searchresults-header').innerText =
           (results.length > count ? 'Over ' : '') + count + ' search results for: ' + term;
@@ -290,24 +252,24 @@ const main = search => {
 
         for (let i = 0; i < count; i++) {
           const resultElem = document.createElement('li');
-          resultElem.innerHTML = formatSearchResult(results[i], terms);
+          resultElem.innerHTML = formatResult(i, results[i], terms);
 
-          searchresults.appendChild(resultElem);
+          ELEMENT_RESULTS.appendChild(resultElem);
         }
 
         // Display results
         document.getElementById('searchresults-outer').classList.remove('hidden');
       };
 
-      const term = searchbar.value.trim();
+      const term = ELEMENT_BAR.value.trim();
 
       if (term != '') {
-        searchbar.classList.add('active');
+        ELEMENT_BAR.classList.add('active');
         doSearch(term);
       } else {
-        searchbar.classList.remove('active');
+        ELEMENT_BAR.classList.remove('active');
         document.getElementById('searchresults-outer').classList.add('hidden');
-        removeChildren(searchresults);
+        removeChildren(ELEMENT_RESULTS);
       }
       marker.unmark();
 
@@ -328,7 +290,7 @@ const main = search => {
       'keyup',
       e => {
         if (e.key != 'Escape') {
-          searchbarKeyUpHandler();
+          keyUpHandler();
           return;
         }
         hiddenSearch();
@@ -337,7 +299,7 @@ const main = search => {
         const tmp = document.createElement('input');
 
         tmp.setAttribute('style', 'position: absolute; opacity: 0;');
-        searchicon.appendChild(tmp);
+        ELEMENT_ICON.appendChild(tmp);
 
         tmp.focus();
         tmp.remove();
@@ -353,9 +315,9 @@ const main = search => {
       if (url.params.hasOwnProperty.call(URL_SEARCH_PARAM) && url.params[URL_SEARCH_PARAM] != '') {
         showSearch();
 
-        searchbar.value = decodeURIComponent((url.params[URL_SEARCH_PARAM] + '').replace(/\+/g, '%20'));
+        ELEMENT_BAR.value = decodeURIComponent((url.params[URL_SEARCH_PARAM] + '').replace(/\+/g, '%20'));
 
-        searchbarKeyUpHandler();
+        keyUpHandler();
       } else {
         hiddenSearch();
       }
@@ -374,9 +336,7 @@ const main = search => {
     };
 
     // If the user uses the browser buttons, do the same as if a reload happened
-    window.onpopstate = () => {
-      doSearchOrMarkFromUrl();
-    };
+    window.onpopstate = () => doSearchOrMarkFromUrl();
 
     // Suppress "submit" events so thje page doesn't reload when the user presses Enter
     document.addEventListener(
@@ -391,25 +351,21 @@ const main = search => {
     doSearchOrMarkFromUrl();
   };
 
-  const filePath = document.getElementById('searcher').dataset.pathtoroot + 'searchindex';
+  const load = document.getElementById('searcher').dataset.pathtoroot + 'searchindex';
 
-  fetch(filePath + '.json')
+  fetch(load + '.json')
     .then(response => response.json())
     .then(json => init(json))
     .catch(() => {
-      // Try to load searchindex.js if fetch failed
+      console.log('Try to load searchindex.js if fetch failed');
       const script = document.createElement('script');
-      script.src = filePath + '.js';
+      script.src = load + '.js';
       script.onload = () => init(search);
       document.head.appendChild(script);
     });
 
-  const hasFocus = () => {
-    return searchbar === document.activeElement;
-  };
-
   // Exported functions
-  search.hasFocus = hasFocus;
+  search.hasFocus = () => ELEMENT_BAR === document.activeElement;
 };
 
 /**
