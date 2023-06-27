7f2357ea22783a0c521a5c13e5ceb8c3524deda4
CoralPink <03_stylist_triages@icloud.com>
2023-06-27 17:27:38 +0900

    Operate the sidebar with grid layout 🐐
    Including the associated JS code and CSS style changes.

--- js/book.js
+++ js/book.js
@@ -1,133 +1,127 @@
 'use strict';
 
-// themes
 (() => {
-  const themePopup = document.getElementById('theme-list');
-  const themeToggleButton = document.getElementById('theme-toggle');
+  const initThemeSelector = () => {
+    const themePopup = document.getElementById('theme-list');
+    const themeToggleButton = document.getElementById('theme-toggle');
 
-  const changeTheme = theme => {
-    themePopup.querySelectorAll('.theme-selected').forEach(el => el.classList.remove('theme-selected'));
+    const changeTheme = theme => {
+      themePopup.querySelectorAll('.theme-selected').forEach(el => el.classList.remove('theme-selected'));
+      themePopup.querySelector('button#' + theme).classList.add('theme-selected');
 
-    themePopup.querySelector('button#' + theme).classList.add('theme-selected');
+      setTimeout(() => {
+        document.querySelector('meta[name="theme-color"]').content = window.getComputedStyle(
+          document.body
+        ).backgroundColor;
+      }, 1);
+    };
 
-    setTimeout(() => {
-      document.querySelector('meta[name="theme-color"]').content = window.getComputedStyle(
-        document.body
-      ).backgroundColor;
-    }, 1);
-  };
+    let currentTheme = (() => {
+      let theme;
 
-  const getTheme = () => {
-    let theme;
-
-    try {
-      theme = localStorage.getItem('mdbook-theme');
-    } catch (_e) {
-      console.log('ERROR: getTheme#mdbook-theme');
-    }
-
-    if (theme != null) {
-      return theme;
-    }
-
-    const defaultTheme = document.getElementById('book').dataset.defaulttheme;
-    changeTheme(defaultTheme);
-
-    return defaultTheme;
-  };
-
-  let currentTheme = getTheme();
-
-  const setTheme = theme => {
-    if (theme == currentTheme) {
-      return;
-    }
-
-    changeTheme(theme);
-
-    const html = document.querySelector('html');
-    html.classList.remove(currentTheme);
-    html.classList.add(theme);
-
-    try {
-      localStorage.setItem('mdbook-theme', theme);
-    } catch (_e) {
-      console.log('ERROR: setTheme#mdbook-theme');
-    }
-
-    currentTheme = theme;
-  };
-
-  const hideThemes = () => {
-    themePopup.style.display = 'none';
-    themeToggleButton.setAttribute('aria-expanded', false);
-    themeToggleButton.focus();
-  };
-
-  const showThemes = () => {
-    themePopup.style.display = 'block';
-    themeToggleButton.setAttribute('aria-expanded', true);
-    themePopup.querySelector('button#' + currentTheme).focus();
-  };
-
-  themeToggleButton.addEventListener(
-    'click',
-    () => {
-      themePopup.style.display === 'block' ? hideThemes() : showThemes();
-    },
-    { once: false, passive: true }
-  );
-
-  themePopup.addEventListener(
-    'click',
-    e => {
-      if (e.target.className === 'theme') {
-        setTheme(e.target.id);
+      try {
+        theme = localStorage.getItem('mdbook-theme');
+      } catch (_e) {
+        console.log('ERROR: getTheme#mdbook-theme');
       }
-    },
-    { once: false, passive: true }
-  );
 
-  themePopup.addEventListener(
-    'focusout',
-    e => {
-      // e.relatedTarget is null in Safari and Firefox on macOS (see workaround below)
-      if (!!e.relatedTarget && !themeToggleButton.contains(e.relatedTarget) && !themePopup.contains(e.relatedTarget)) {
-        hideThemes();
+      if (theme != null) {
+        return theme;
       }
-    },
-    { once: false, passive: true }
-  );
 
-  // Should not be needed, but it works around an issue on macOS & iOS: https://github.com/rust-lang/mdBook/issues/628
-  document.addEventListener(
-    'click',
-    e => {
-      if (
-        themePopup.style.display === 'block' &&
-        !themeToggleButton.contains(e.target) &&
-        !themePopup.contains(e.target)
-      ) {
-        hideThemes();
+      const defaultTheme = document.getElementById('book').dataset.defaulttheme;
+      changeTheme(defaultTheme);
+
+      return defaultTheme;
+    })();
+
+    const setTheme = theme => {
+      if (theme == currentTheme) {
+        return;
       }
-    },
-    { once: false, passive: true }
-  );
 
-  document.addEventListener(
-    'keydown',
-    e => {
-      if (themePopup.contains(e.target)) {
-        e.preventDefault();
-        hideThemes();
+      changeTheme(theme);
+
+      const html = document.querySelector('html');
+      html.classList.remove(currentTheme);
+      html.classList.add(theme);
+
+      try {
+        localStorage.setItem('mdbook-theme', theme);
+      } catch (_e) {
+        console.log('ERROR: setTheme#mdbook-theme');
       }
-    },
-    { once: false, passive: false }
-  );
-})();
 
-(() => {
-  const main = document.querySelector('main');
+      currentTheme = theme;
+    };
+
+    const hideThemes = () => {
+      themePopup.style.display = 'none';
+      themeToggleButton.setAttribute('aria-expanded', false);
+      themeToggleButton.focus();
+    };
+
+    const showThemes = () => {
+      themePopup.style.display = 'block';
+      themeToggleButton.setAttribute('aria-expanded', true);
+      themePopup.querySelector('button#' + currentTheme).focus();
+    };
+
+    themeToggleButton.addEventListener(
+      'click',
+      () => {
+        themePopup.style.display === 'block' ? hideThemes() : showThemes();
+      },
+      { once: false, passive: true }
+    );
+
+    themePopup.addEventListener(
+      'click',
+      e => {
+        if (e.target.className === 'theme') {
+          setTheme(e.target.id);
+        }
+      },
+      { once: false, passive: true }
+    );
+
+    themePopup.addEventListener(
+      'focusout',
+      e => {
+        // e.relatedTarget is null in Safari and Firefox on macOS (see workaround below)
+        if (!!e.relatedTarget && !themeToggleButton.contains(e.relatedTarget) && !themePopup.contains(e.relatedTarget)) {
+          hideThemes();
+        }
+      },
+      { once: false, passive: true }
+    );
+
+    // Should not be needed, but it works around an issue on macOS & iOS: https://github.com/rust-lang/mdBook/issues/628
+    document.addEventListener(
+      'click',
+      e => {
+        if (
+          themePopup.style.display === 'block' &&
+          !themeToggleButton.contains(e.target) &&
+          !themePopup.contains(e.target)
+        ) {
+          hideThemes();
+        }
+      },
+      { once: false, passive: true }
+    );
+
+    document.addEventListener(
+      'keydown',
+      e => {
+        if (themePopup.contains(e.target)) {
+          e.preventDefault();
+          hideThemes();
+        }
+      },
+      { once: false, passive: false }
+    );
+  };
 
   // chapterNavigation
   document.addEventListener(
@@ -136,6 +130,8 @@
       if (window.search.hasFocus()) {
         return;
       }
+
+      const main = document.querySelector('.content main');
 
       if (e.key == 'ArrowRight') {
         e.preventDefault();
@@ -159,6 +155,8 @@
   );
 
   const initCodeBlock = () => {
+    const main = document.querySelector('.content main');
+
     Array.from(main.querySelectorAll('code'))
       // Don't highlight `inline code` blocks in headers.
       .filter(node => !node.parentElement.classList.contains('header'))
@@ -230,13 +228,9 @@
   };
 
   const initSideBar = () => {
-    const html = document.querySelector('html');
+    const page = document.getElementById('page');
     const sidebar = document.getElementById('sidebar');
-    const sidebarToggleButton = document.getElementById('sidebar-toggle');
-
-    // Apply ARIA attributes after the sidebar and the sidebar toggle button are added to the DOM
-    sidebarToggleButton.setAttribute('aria-expanded', sidebar === 'visible');
-    sidebar.setAttribute('aria-hidden', sidebar !== 'visible');
+    const toggleButton = document.getElementById('sidebar-toggle');
 
     const toggleSection = ev => {
       ev.currentTarget.parentElement.classList.toggle('expanded');
@@ -247,15 +241,10 @@
     });
 
     const showSidebar = () => {
-      if (html.classList.contains('sidebar-visible')) {
-        return;
-      }
-
-      html.classList.remove('sidebar-hidden');
-      html.classList.add('sidebar-visible');
-
-      sidebarToggleButton.setAttribute('aria-expanded', true);
+      page.style.display = 'grid';
+      sidebar.style.display = 'block';
       sidebar.setAttribute('aria-hidden', false);
+      toggleButton.setAttribute('aria-expanded', true);
 
       try {
         localStorage.setItem('mdbook-sidebar', 'visible');
@@ -265,15 +254,10 @@
     };
 
     const hideSidebar = () => {
-      if (html.classList.contains('sidebar-hidden')) {
-        return;
-      }
-
-      html.classList.remove('sidebar-visible');
-      html.classList.add('sidebar-hidden');
-
-      sidebarToggleButton.setAttribute('aria-expanded', false);
+      page.style.display = 'block';
+      sidebar.style.display = 'none';
       sidebar.setAttribute('aria-hidden', true);
+      toggleButton.setAttribute('aria-expanded', false);
 
       try {
         localStorage.setItem('mdbook-sidebar', 'hidden');
@@ -282,11 +266,13 @@
       }
     };
 
+    toggleButton.getAttribute('aria-expanded') == 'true' ? hideSidebar() : showSidebar();
+
     // Toggle sidebar
-    sidebarToggleButton.addEventListener(
+    toggleButton.addEventListener(
       'click',
       () => {
-        html.classList.contains('sidebar-hidden') ? showSidebar() : hideSidebar();
+        toggleButton.getAttribute('aria-expanded') == 'true' ? hideSidebar() : showSidebar();
       },
       { once: false, passive: true }
     );
@@ -315,11 +301,27 @@
       },
       { once: false, passive: true }
     );
+
     // FIXME: The definitions are all over the place.
     if (window.innerWidth < 750) {
       hideSidebar();
+      return;
     }
 
+    const getSidebarState = () => {
+      const sidebar = localStorage.getItem('mdbook-sidebar');
+
+      if (sidebar) {
+        return sidebar;
+      }
+
+      // FIXME: The definitions are all over the place.
+      return document.body.clientWidth < 750 ? 'hidden' : 'visible';
+    };
+    getSidebarState() == 'visible' ? showSidebar() : hideSidebar();
+  }
+
+  const touchControl = () => {
     let firstContact = null;
 
     document.addEventListener(
@@ -432,6 +434,8 @@
       initSideBar();
       initCodeBlock();
       createTableOfContents();
+      initThemeSelector();
+      touchControl();
     },
     { once: true, passive: true }
   );
