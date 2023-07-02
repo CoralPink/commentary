3cf86a2b3eaba4a2a13ec408f8207664ec662fdb
CoralPink <03_stylist_triages@icloud.com>
2023-07-02 13:41:46 +0900

    Various CSS layout adjustments 🦢
    🦆 Fixed an issue where the background color of keywords was lost.
    🦆 Change from specifying px to rem as much as possible.
    🦆 Fine-tuning of design.
    🦆 Removed unnecessary parameters.

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v0.1.0';
+const CACHE_NAME = 'v0.1.1';
 
 const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
   // First try to get the resource from the cache
