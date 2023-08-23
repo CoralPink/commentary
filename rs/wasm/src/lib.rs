fc93763d31c65d0c193039643fa76f0e706a3906
CoralPink <03_stylist_triages@icloud.com>
2023-08-23 18:12:32 +0900

    Replace it with more practical code 🌾
    I'm not sure if there is any advantage over JavaScript,
    but if it's fun, that's all that matters! Elegant, right? 😆

--- rs/wasm/src/lib.rs
+++ rs/wasm/src/lib.rs
@@ -1,10 +1,26 @@
+use js_sys::Array;
 use wasm_bindgen::prelude::*;
+use web_sys::NodeList;
+
+fn node_list_to_array(node_list: NodeList) -> Array {
+    Array::from(&node_list)
+}
 
 #[wasm_bindgen]
-pub fn draw() {
+pub fn attribute_external_links() {
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
 
-    let app = document.get_element_by_id("wasm-test").unwrap();
-    app.set_inner_html("<p>ごめんなさい...。このURLは無効です...🙀</p>");
+    let elements = document
+        .query_selector_all(r#".content main a[href^="http"]"#)
+        .unwrap();
+
+    let elements_array = node_list_to_array(elements);
+
+    for el in elements_array.iter() {
+        if let Some(el) = el.dyn_ref::<web_sys::Element>() {
+            el.set_attribute("target", "_blank").unwrap();
+            el.set_attribute("rel", "noopener").unwrap();
+        }
+    }
 }
