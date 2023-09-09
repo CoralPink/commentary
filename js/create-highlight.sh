85810678f9e566525953c4b69924d7ac35dda41f
CoralPink <03_stylist_triages@icloud.com>
2023-09-09 13:01:34 +0900

    chore: various updates. 🧈
    note:
    `highlight.js` is warning you, but it probably won't be a problem.

--- js/create-highlight.sh
+++ js/create-highlight.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/highlightjs/highlight.js.git -b 11.8.0 --depth 1
 cd highlight.js
 
-npm install
-node tools/build.js -n bash diff json lua plaintext vim
+bun install
+bun tools/build.js -n bash diff json lua plaintext vim
 
 cd ..
