b34152e3f5b447b2392b6044cdd42b9089611199
CoralPink <03_stylist_triages@icloud.com>
2023-11-17 15:06:34 +0900

    Change `String` to `&str` for efficiency 🗡️

--- rs/wasm/src/searcher.rs
+++ rs/wasm/src/searcher.rs
@@ -157,22 +157,27 @@ impl Teaser {
     }
 }
 
-#[wasm_bindgen]
-pub fn format_result(
-    path_to_root: String,
-    link_uri: String,
-    doc_body: String,
-    doc_breadcrumbs: String,
-    term: String,
-    count: usize,
-) -> String {
+fn uri_parser(link_uri: &str) -> (&str, String) {
     let uri: Vec<&str> = link_uri.split('#').collect();
-    let page = uri[0];
     let head = if uri.len() > 1 {
         format!("#{}", uri[1])
     } else {
         "".to_owned()
     };
+
+    (uri[0], head)
+}
+
+#[wasm_bindgen]
+pub fn format_result(
+    path_to_root: &str,
+    link_uri: &str,
+    doc_body: &str,
+    doc_breadcrumbs: &str,
+    term: &str,
+    count: usize,
+) -> String {
+    let (page, head) = uri_parser(link_uri);
 
     format!(
         r#"<a href="{path_to_root}{page}?highlight={}{head}">{doc_breadcrumbs}</a><span class="teaser" aria-label="Search Result Teaser">{}</span>"#,
@@ -181,7 +186,7 @@ pub fn format_result(
             .unwrap()
             .replace('\'', "%27"),
         Teaser::new().search_result_excerpt(
-            &doc_body,
+            doc_body,
             term.split(' ').map(|s| s.to_string()).collect(),
             count,
         )
