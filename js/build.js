8666b5975a5f312aafa360d391072a53efd161b8
CoralPink <03_stylist_triages@icloud.com>
2023-11-25 15:55:40 +0900

    Make it fun to cook like a `wasm-pack` 👩🏼‍🍳

--- js/build.js
+++ js/build.js
@@ -1,3 +1,11 @@
+const CLR_RESET = "\x1b[0m";
+const CLR_BC = "\x1b[1;35m";
+const CLR_BG = "\x1b[1;32m";
+const CLR_Y = "\x1b[33m";
+
+console.info(`[INFO]: 👩🏼‍🍳 I'm going to bake ${CLR_BC}bun${CLR_RESET} now!`);
+const start = performance.now();
+
 const result = await Bun.build({
   entrypoints: ['./book.js', './hl-worker.js', './searcher.js', './serviceworker.js'],
   outdir: './dist',
@@ -11,3 +19,6 @@ if (!result.success) {
 for (const message of result.logs) {
   console.log(message);
 }
+
+const time = Math.floor(performance.now() - start) / 1000;
+console.info(`    ${CLR_BG}Finished${CLR_RESET} release target(s) in ${time}s 😋${CLR_Y}Yummy!!${CLR_RESET}`);
