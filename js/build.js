5bcbe9fdf85e3b5bc54d5f432352fa17f2ba1a77
CoralPink <03_stylist_triages@icloud.com>
2023-11-25 14:38:02 +0900

    Output build log 🍋
    Throws an `error` in case of failure.

--- js/build.js
+++ js/build.js
@@ -1,5 +1,13 @@
-await Bun.build({
+const result = await Bun.build({
   entrypoints: ['./book.js', './hl-worker.js', './searcher.js', './serviceworker.js'],
   outdir: './dist',
   minify: true,
 });
+
+if (!result.success) {
+  throw new AggregateError(result.logs, 'Build Failed');
+}
+
+for (const message of result.logs) {
+  console.log(message);
+}
