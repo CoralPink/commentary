e87c0969c85810da5d4dee63cd1a44114dea3958
CoralPink <03_stylist_triages@icloud.com>
2023-12-15 16:38:24 +0900

    Remove the ability to move pages with the keys 🗝️

--- js/book.js
+++ js/book.js
@@ -7,32 +7,6 @@ import ThemeSelector from './theme-selector.js';
 
 import './sidebar.js';
 
-const chapterNavigation = () => {
-  document.addEventListener(
-    'keyup',
-    e => {
-      if (globalThis.search.hasFocus()) {
-        return;
-      }
-
-      if (e.key === 'ArrowRight') {
-        const nextButton = document.getElementById('main').querySelector('.nav-chapters.next');
-
-        if (nextButton) {
-          globalThis.location.href = nextButton.href;
-        }
-      } else if (e.key === 'ArrowLeft') {
-        const previousButton = document.getElementById('main').querySelector('.nav-chapters.previous');
-
-        if (previousButton) {
-          globalThis.location.href = previousButton.href;
-        }
-      }
-    },
-    { once: false, passive: true },
-  );
-};
-
 wasmInit()
   .then(() => {
     attribute_external_links();
@@ -48,8 +22,6 @@ document.addEventListener(
 
     new TableOfContents();
     new ThemeSelector();
-
-    chapterNavigation();
   },
   { once: true, passive: true },
 );
