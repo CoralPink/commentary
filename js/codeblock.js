f5c2d3af60b517ab88c04022ea38a9510fc0c320
CoralPink <03_stylist_triages@icloud.com>
2023-12-15 17:05:14 +0900

    More efficient code highlighting process 🍄 (#71)

--- js/codeblock.js
+++ js/codeblock.js
@@ -1,49 +1,58 @@
 const WORKER_PATH = '/commentary/hl-worker.js';
 const MAX_THREAD = 16;
 
+const POP_WAIT = 10;
+const TIME_OUT = 1000;
+
 // Singleton Class
 class WorkerPool {
   static _instance;
+  #array = [];
 
-  #array;
-  #count;
-
-  constructor() {
+  constructor(threadNum = 0) {
     if (WorkerPool._instance) {
       return WorkerPool._instance;
     }
-    this.#array = [];
-    this.#count = 0;
+
+    for (let i = 0; i < Math.min(threadNum, MAX_THREAD); i++) {
+      this.#array.push(new Worker(WORKER_PATH));
+    }
+
     WorkerPool._instance = this;
   }
 
-  push(worker) {
-    if (worker !== undefined) {
-      this.#array.push(worker);
-      return;
-    }
-    this.#array.push(new Worker(WORKER_PATH));
-    this.#count++;
+  push(thread) {
+    this.#array.push(thread);
   }
 
   async pop() {
-    if (this.#count < MAX_THREAD) {
-      this.push();
-    }
+    let retry = true;
 
-    const start = performance.now();
+    const workerPromise = new Promise(resolve => {
+      const checkAndPop = () => {
+        const worker = this.#array.pop();
 
-    do {
-      const worker = this.#array.pop();
+        if (worker !== undefined) {
+          resolve(worker);
+          return;
+        }
 
-      if (worker !== undefined) {
-        return worker;
-      }
+        if (retry) {
+          setTimeout(checkAndPop, POP_WAIT);
+        }
+      };
 
-      await new Promise(resolve => setTimeout(resolve, 10));
-    } while (performance.now() - start < 1000);
+      checkAndPop();
+    });
 
-    throw new Error('Pool pop time out!');
+    const timeOutPromise = new Promise((_, reject) => {
+      setTimeout(() => {
+        retry = false;
+        reject(new Error('Pool pop time out!'));
+      }, TIME_OUT);
+    });
+
+    return await Promise.race([workerPromise, timeOutPromise]);
   }
 
   release() {
@@ -51,7 +60,6 @@ class WorkerPool {
       worker.terminate();
     }
     this.#array.length = 0;
-    this.#count = 0;
   }
 }
 
@@ -83,7 +91,9 @@ export const codeBlock = async () => {
     return;
   }
 
-  const codeQuery = main.querySelectorAll('pre code');
+  const codeQuery = Array.from(main.querySelectorAll('pre code')).filter(
+    code => !code.classList.contains('language-txt'),
+  );
 
   if (codeQuery.length <= 0) {
     return;
@@ -100,30 +110,27 @@ export const codeBlock = async () => {
   clip.setAttribute('aria-label', 'Copy to clipboard');
   clip.innerHTML = '<i class="tooltiptext"></i>';
 
-  const workerPool = new WorkerPool();
+  const workerPool = new WorkerPool(codeQuery.length);
 
   for (const code of codeQuery) {
-    if (code.classList.contains('language-txt')) {
-      continue;
-    }
+    workerPool
+      .pop()
+      .then(worker => {
+        worker.onmessage = ev => {
+          code.innerHTML = ev.data;
+          workerPool.push(worker);
+        };
 
-    try {
-      const worker = await workerPool.pop();
+        worker.onerror = e => {
+          console.error(`Error codeBlock(): ${e}`);
+          workerPool.push(worker);
+        };
 
-      worker.onmessage = ev => {
-        code.innerHTML = ev.data;
-        workerPool.push(worker);
-      };
-
-      worker.onerror = e => {
-        console.error(`Error codeBlock(): ${e}`);
-        workerPool.push(worker);
-      };
-
-      worker.postMessage([code.textContent, code.classList[0]]);
-    } catch (e) {
-      console.error(e);
-    }
+        worker.postMessage([code.textContent, code.classList[0]]);
+      })
+      .catch(e => {
+        console.error(e);
+      });
 
     const buttons = document.createElement('div');
     buttons.className = 'buttons';
--- js/serviceworker.js
+++ js/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v0.13.0';
+const CACHE_VERSION = 'v0.13.1';
 const CACHE_LIST = [
   '/commentary/book.js',
   '/commentary/elasticlunr.min.js',
