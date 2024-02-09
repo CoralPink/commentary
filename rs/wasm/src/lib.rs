0b257d9160d3ea66469731f25278d215f251b5b6
CoralPink <03_stylist_triages@icloud.com>
2024-02-09 18:11:31 +0900

    Organize the code 🍣

--- rs/wasm/src/lib.rs
+++ rs/wasm/src/lib.rs
@@ -1,5 +1,2 @@
-#[macro_use]
-extern crate serde_derive;
-
 pub mod book;
 pub mod searcher;
--- rs/wasm/src/searcher/mod.rs
+++ rs/wasm/src/searcher/mod.rs
@@ -1,9 +1,10 @@
 use js_sys::Array;
+use serde::Deserialize;
 use wasm_bindgen::prelude::*;
 use web_sys::{Document, Element};
 
-pub mod teaser;
-pub use crate::searcher::teaser::Teaser;
+mod teaser;
+use crate::searcher::teaser::Teaser;
 
 #[wasm_bindgen]
 extern "C" {
@@ -90,6 +91,8 @@ impl SearchResult {
             .expect("Failed to deserialize JsValue to Vec<ResultObject>");
 
         elements.into_iter().for_each(|el| {
+            self.teaser.clear();
+
             let idx = match el.key.parse::<usize>() {
                 Ok(n) => n,
                 Err(_) => {
@@ -97,29 +100,27 @@ impl SearchResult {
                     return;
                 }
             };
+
             let (page, head) = parse_uri(&self.url_table[idx]);
             let terms = term.split_whitespace().collect::<Vec<&str>>();
+
+            let highlight = js_sys::encode_uri_component(&terms.join("%20"))
+                .as_string()
+                .unwrap_or_default()
+                .replace('\'', "%27");
+
+            let teaser = self
+                .teaser
+                .search_result_excerpt(&el.doc.body, terms, self.count);
 
             let new_element = self
                 .document
                 .create_element("li")
                 .expect("failed: create <li>");
 
-            self.teaser.clear();
-
             new_element.set_inner_html(&format!(
-              r#"<a href="{}{page}?highlight={}#{head}">{}</a><span class="teaser" aria-label="Search Result Teaser">{}</span>"#,
-              &self.path_to_root,
-              js_sys::encode_uri_component(&terms.join("%20"))
-                  .as_string()
-                  .unwrap_or_default()
-                  .replace('\'', "%27"),
-              el.doc.breadcrumbs,
-              self.teaser.search_result_excerpt(
-                  &el.doc.body,
-                  terms,
-                  self.count,
-              )
+                r#"<a href="{}{}?highlight={}#{}">{}</a><span class="teaser" aria-label="Search Result Teaser">{}</span>"#,
+                &self.path_to_root, page, highlight, head, el.doc.breadcrumbs, teaser
             ));
 
             self.parent
