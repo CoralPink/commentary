8b0b3c84e1bd44eda031afa3fd291576be25998c
CoralPink <03_stylist_triages@icloud.com>
2024-02-10 23:56:46 +0900

    fix: strange code 🎣 - Rename functions - remove unnecessary variables

--- js/finder.js
+++ js/finder.js
@@ -17,12 +17,10 @@ export default class Finder {
     });
   }
 
-  #findFirstIndex(array) {
+  #findScoreBoundaries(array) {
     let low = 0;
     let high = array.length - 1;
     let resultIndex = -1;
-
-    let count = 0;
 
     while (low <= high) {
       const mid = Math.floor((low + high) / 2);
@@ -33,7 +31,6 @@ export default class Finder {
       } else {
         high = mid - 1;
       }
-      count += 1;
     }
     return resultIndex !== -1 ? resultIndex : low;
   }
@@ -42,7 +39,7 @@ export default class Finder {
     const results = this.#fzf.find(term);
 
     return results
-      .slice(0, this.#findFirstIndex(results) + 1)
+      .slice(0, this.#findScoreBoundaries(results) + 1)
       .map(x => ({ doc: this.#storeDocs[x.item], key: x.item, score: x.score }));
   }
 }
