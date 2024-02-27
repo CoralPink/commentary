3608440e7038fb35b0cbd4508f40ca313c5f8c9a
CoralPink <03_stylist_triages@icloud.com>
2024-02-27 14:16:45 +0900

    Improve inefficient processes ⛸️

--- rs/wasm/src/mark/mod.rs
+++ rs/wasm/src/mark/mod.rs
@@ -3,59 +3,6 @@ use web_sys::{Element, Node, NodeList};
 
 const NODE_TYPE: u16 = 3;
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-#[allow(unused_macros)]
-macro_rules! console_log {
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-
-// I'm making a very out-of-the-box decision.
-// However, at present, I don't see any better solution....
-fn is_text(input: &str) -> bool {
-    !input.trim_start().starts_with('<')
-}
-
-fn replace(node: &Node, term: &str) {
-    if let Some(elm) = node.parent_element() {
-        if elm.tag_name() == "MARK" {
-            return;
-        }
-
-        let inner = elm.inner_html();
-
-        if is_text(inner.as_str()) {
-            if inner.contains("<mark>") {
-                return;
-            }
-            let marked = term.split_whitespace().fold(inner.clone(), |acc, word| {
-                acc.replace(word, &format!("<mark>{}</mark>", word))
-            });
-
-            elm.set_inner_html(&marked);
-        }
-    }
-}
-
-fn process_nodes(node: &Node, term: &str) {
-    if node.node_type() == NODE_TYPE {
-        replace(node, term);
-        return;
-    }
-
-    if let Some(elm) = node.dyn_ref::<Element>() {
-        for i in 0..elm.child_nodes().length() {
-            if let Some(child) = elm.child_nodes().item(i) {
-                process_nodes(&child, term);
-            }
-        }
-    }
-}
-
 fn node_list_to_vec(node_list: NodeList) -> Vec<Element> {
     let mut vec = Vec::new();
 
@@ -69,16 +16,48 @@ fn node_list_to_vec(node_list: NodeList) -> Vec<Element> {
     vec
 }
 
+fn replace(node: &Node, term: &str) {
+    if let Some(elm) = node.parent_element() {
+        if elm.tag_name() == "MARK" {
+            return;
+        }
+
+        let inner = elm.inner_html();
+
+        // I'm making a very out-of-the-box decision.
+        // However, at present, I don't see any better solution....
+        if inner.as_str().trim_start().starts_with('<') {
+            return;
+        }
+
+        let marked = term.split_whitespace().fold(inner.clone(), |acc, word| {
+            acc.replace(word, &format!("<mark>{}</mark>", word))
+        });
+
+        elm.set_inner_html(&marked);
+    }
+}
+
+fn process_nodes(node: &Node, term: &str) {
+    if let Some(elm) = node.dyn_ref::<Element>() {
+        if let Some(last) = elm
+            .child_nodes()
+            .item(elm.child_nodes().length().wrapping_sub(1))
+        {
+            if last.node_type() == NODE_TYPE {
+                replace(&last, term);
+            }
+        }
+    }
+}
+
 #[wasm_bindgen]
 pub fn marking(term: &str) {
     let window = web_sys::window().expect("window not available");
     let document = window.document().expect("document not available");
 
     if let Some(main) = document.get_element_by_id("main") {
-        for node in node_list_to_vec(
-            main.query_selector_all("*")
-                .expect("Failed to query elements"),
-        ) {
+        for node in node_list_to_vec(main.query_selector_all("*").expect("Failed: marking")) {
             process_nodes(&node, term);
         }
     }
