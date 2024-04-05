d83878bb6aed6f321d35b210421de722b5ea4487
CoralPink <03_stylist_triages@icloud.com>
2024-04-05 15:57:49 +0900

    I knew I should have fixed it properly 🐖

--- js/hl-worker.js
+++ js/hl-worker.js
@@ -1,7 +1,6 @@
 import { highlight } from './highlight.js/build/highlight.js';
 
-/* biome-ignore lint: no-global-assign */
-onmessage = ev => {
+self.onmessage = ev => {
   postMessage(
     highlight(ev.data[0], {
       language: ev.data[1].slice(9), // Remove the string "language-" before passing.
