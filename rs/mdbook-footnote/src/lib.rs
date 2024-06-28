fc68b412d4fb2d00bf683479e30fa13c83eb9d20
CoralPink <03_stylist_triages@icloud.com>
2024-06-28 15:24:42 +0900

    add: test for `mdbook-footnote` 👣
    Well, it's a bit late now 🤷‍♀️

--- rs/mdbook-footnote/src/lib.rs
+++ rs/mdbook-footnote/src/lib.rs
@@ -36,3 +36,62 @@ pub fn replacing(mut book: Book) -> Result<Book, Error> {
 
     Ok(book)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::replacing;
+    use mdbook::book::{Book, BookItem, Chapter};
+    use pretty_assertions::assert_eq;
+    use std::{fs, fs::File, io::Write};
+
+    const CLR_RESET: &str = "\x1b[0m";
+    const CLR_R: &str = "\x1b[31m";
+    const CLR_C: &str = "\x1b[36m";
+
+    const TEST_DIR: &str = "test/";
+    const TEST_MD: &str = "test.md";
+
+    const OK_RESULT: &str = "ok.md";
+    const OUTPUT_RESULT: &str = "result.md";
+
+    fn write_chapters_to_files(chap: &Chapter) -> Result<(), Box<dyn std::error::Error>> {
+        let mut file = File::create(String::from(TEST_DIR) + OUTPUT_RESULT)?;
+        file.write_all(chap.content.as_bytes())?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_footnote() {
+        let mut book = Book::new();
+
+        book.push_item(BookItem::Chapter(Chapter::new(
+            "Test Chapter",
+            fs::read_to_string(String::from(TEST_DIR) + TEST_MD).unwrap(),
+            std::path::Path::new(TEST_MD),
+            vec![],
+        )));
+
+        println!(
+            "{CLR_C}[INFO]{CLR_RESET} Depending on the test case, [WARNING] may be displayed."
+        );
+
+        match replacing(book) {
+            Ok(book) => {
+                for item in book.iter() {
+                    if let BookItem::Chapter(chap) = item {
+                        write_chapters_to_files(chap)
+                            .unwrap_or_else(|err| panic!("{CLR_R}ERROR{CLR_RESET}: {err}"));
+                        assert_eq!(
+                            chap.content,
+                            fs::read_to_string(String::from(TEST_DIR) + OK_RESULT).unwrap()
+                        );
+                    }
+                }
+            }
+            Err(err) => {
+                panic!("ERROR: {err}");
+            }
+        }
+    }
+}
---
+++ rs/mdbook-footnote/test/ok.md
@@ -0,0 +1,30 @@
+# 🎠 Wathing the Wheels
+
+```admonish success title=""
+I'm just sitting here watching the wheels<sup class="footnote-reference"><a name="to-footnote-1">[1](#1)</a></sup>go 'round and 'round
+
+I really love to watch them roll
+
+ただここに座って車輪がぐるぐると進むのを眺めてる
+
+回ってるのを見るのが大好きなんだ
+```
+
+```admonish success title=""
+No longer riding on the merry-go-round
+
+周ってるだけのメリーゴーラウンドには乗れなくなった
+```
+
+```admonish success
+I just had to let it go
+
+ぼくはもう手放すしかなかったんだ
+```
+<div class="footnote-definition" id=1>
+
+[<sup>1:</sup>](#to-footnote-1) Wathing the Wheels (by [John Lennon](https://en.m.wikipedia.org/wiki/John_Lennon)):
+Lennon と Ono のアルバム[Double Fantasy](https://en.m.wikipedia.org/wiki/Double_Fantasy)からリリースされた、3枚目にして最後のシングル。
+Yoko との息子 Sean の子育てに専念するため、
+音楽業界から引退した1975年から1980年の "househusband (主夫)" 時代、自分に対して戸惑いの目を向ける人々に語りかけている。
+</div>
