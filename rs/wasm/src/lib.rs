792a39f8179963650d5ec17b51b75c2827abee4e
CoralPink <03_stylist_triages@icloud.com>
2024-07-23 16:37:59 +0900

    Revert "Improving `CI` part2 🎪🎪 (#141)"
    This reverts commit cfa44306baa4e1024194ead6a0cb15a0d4200a94.

--- rs/wasm/src/lib.rs
+++ rs/wasm/src/lib.rs
@@ -1,13 +1,3 @@
 pub mod link;
 pub mod mark;
 pub mod searcher;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_wasm() {
-        println!(
-            "No test currently exists..."
-        );
-    }
-}
