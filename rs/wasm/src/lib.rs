cfa44306baa4e1024194ead6a0cb15a0d4200a94
CoralPink <03_stylist_triages@icloud.com>
2024-07-23 16:27:45 +0900

    Improving `CI` part2 🎪🎪 (#141)
    * Improving `CI` part2 🎪🎪
    
    * Use artifacts.
    
    * Perhaps the `cp` command is not working, so fix it.
    
    * I may be wrong about a lot of things...
    
    * I think the file is being overwritten and erased...
    
    * Well, that's right.
    
    * Well, that's right. part2
    
    * Overall revision and re-structuring.
    
    * "Upload SCSS" -> "Upload CSS".
    
    * View all files.
    
    * Perhaps I was sleepwalking and removed the `id`...
    
    * Remove `CARGO_TERM_COLOR` as it does not seem particularly necessary.
    
    * Remove `mkdir` as it does not seem particularly necessary.
    
    * Remove options that seem useless.
    
    * Why is style.css missing...?
    
    * Why is style.css missing...? part2
    
    * Why is style.css missing...? part3
    
    * Wrong cp command...?
    
    * Okay, that settles it!
    
    * add: if-no-files-found
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * add: `if-no-files-found` part2
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- rs/wasm/src/lib.rs
+++ rs/wasm/src/lib.rs
@@ -1,3 +1,13 @@
 pub mod link;
 pub mod mark;
 pub mod searcher;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_wasm() {
+        println!(
+            "No test currently exists..."
+        );
+    }
+}
