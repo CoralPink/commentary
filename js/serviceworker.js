08610802bdc991ac92798db61e2de8414aab010a
CoralPink <03_stylist_triages@icloud.com>
2024-09-17 12:00:31 +0900

    refactor: `Service worker` 👩‍💼
    (not accompanied by a change in the cache version.)

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -1,6 +1,8 @@
 const CACHE_VERSION = 'v2.0.1';
 
+const CACHE_HOST = 'https://coralpink.github.io/';
 const CACHE_URL = '/commentary/';
+
 const CACHE_LIST = [
   'book.js',
   'hl-worker.js',
@@ -24,51 +26,7 @@ const CACHE_LIST = [
   'woff/NerdFontsSymbolsOnly/SymbolsNerdFontMono-Regular.woff2',
 ];
 
-const CACHE_USE = ['https://coralpink.github.io/', 'http://127.0.0.1:8080/'];
-
-const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-  // First try to get the resource from the cache
-  const responseFromCache = await caches.match(request);
-
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-
-  // Next try to use the preloaded response, if it's there
-  const preloadResponse = await preloadResponsePromise;
-
-  const putInCache = async (request, response) => {
-    const cache = await caches.open(CACHE_VERSION);
-    await cache.put(request, response);
-  };
-
-  if (preloadResponse) {
-    putInCache(request, preloadResponse.clone());
-    return preloadResponse;
-  }
-
-  // Next try to get the resource from the network
-  try {
-    const responseFromNetwork = await fetch(request);
-    // response may be used only once
-    // we need to save clone to put one copy in cache
-    // and serve second one
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
-  } catch (error) {
-    const fallbackResponse = await caches.match(fallbackUrl);
-    if (fallbackResponse) {
-      return fallbackResponse;
-    }
-    // when even the fallback response is not available,
-    // there is nothing we can do, but we must always
-    // return a Response object
-    return new Response('Network error happened', {
-      status: 408,
-      headers: { 'Content-Type': 'text/plain' },
-    });
-  }
-};
+const FALLBACK_URL = `${CACHE_URL}chrome-96x96.png`;
 
 const deleteCache = async key => {
   await caches.delete(key);
@@ -81,18 +39,22 @@ const deleteOldCaches = async () => {
   await Promise.all(cachesToDelete.map(deleteCache));
 };
 
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
+addEventListener(
+  'activate',
+  event => {
+    event.waitUntil(clients.claim());
 
-  event.waitUntil(
-    (async () => {
-      if (self.registration.navigationPreload) {
-        await self.registration.navigationPreload.enable();
-      }
-    })(),
-  );
-  event.waitUntil(deleteOldCaches());
-}, { once: false, passive: true });
+    event.waitUntil(
+      (async () => {
+        if (self.registration.navigationPreload) {
+          await self.registration.navigationPreload.enable();
+        }
+      })(),
+    );
+    event.waitUntil(deleteOldCaches());
+  },
+  { once: false, passive: true },
+);
 
 const extractVersionParts = cacheName => {
   const versionString = cacheName.substring(1);
@@ -109,42 +71,82 @@ const shouldSkipWaiting = cacheList => {
   });
 };
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    (async () => {
-      if (shouldSkipWaiting(await caches.keys())) {
-        self.skipWaiting();
-      }
-      const cache = await caches.open(CACHE_VERSION);
-      await cache.addAll(CACHE_LIST.map(x => CACHE_URL + x));
-    })(),
-  );
-}, { once: false, passive: true });
+addEventListener(
+  'install',
+  event => {
+    event.waitUntil(
+      (async () => {
+        if (shouldSkipWaiting(await caches.keys())) {
+          self.skipWaiting();
+        }
+        const cache = await caches.open(CACHE_VERSION);
+        await cache.addAll(CACHE_LIST.map(x => CACHE_URL + x));
+      })(),
+    );
+  },
+  { once: false, passive: true },
+);
 
-self.addEventListener('fetch', async event => {
-  for (const x of CACHE_USE) {
-    if (!event.request.url.startsWith(x)) {
-      continue;
+const putInCache = async (request, response) => {
+  const cache = await caches.open(CACHE_VERSION);
+  await cache.put(request, response);
+};
+
+const cacheFirst = async (request, preloadResponse) => {
+  // First try to get the resource from the cache
+  const responseFromCache = await caches.match(request);
+
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // Next try to use the preloaded response, if it's there
+  const preloadResponsePromise = await preloadResponse;
+
+  if (preloadResponsePromise) {
+    putInCache(request, preloadResponsePromise.clone());
+    return preloadResponsePromise;
+  }
+
+  // Next try to get the resource from the network
+  try {
+    const responseFromNetwork = await fetch(request);
+
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    const fallbackResponse = await caches.match(FALLBACK_URL);
+
+    if (fallbackResponse) {
+      return fallbackResponse;
     }
 
-    const preloadResponsePromise = event.preloadResponse;
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+};
+
+addEventListener(
+  'fetch',
+  event => {
+    if (!event.request.url.startsWith(CACHE_HOST)) {
+      return;
+    }
 
     event.respondWith(
       (async () => {
-        const preloadResponse = await preloadResponsePromise;
-
-        // Non-blocking cacheFirst function
-        const cachedResponse = await cacheFirst({
-          request: event.request,
-          preloadResponsePromise: preloadResponse,
-          fallbackUrl: `${CACHE_URL}chrome-96x96.png`,
-        });
-
+        const cachedResponse = await cacheFirst(event.request, event.preloadResponse);
         return cachedResponse;
       })(),
     );
-
-    // Ensure that the event does not finish until the respondWith promise settles
-    event.waitUntil(preloadResponsePromise);
-  }
-}, { once: false, passive: true });
+  },
+  { once: false, passive: true },
+);
