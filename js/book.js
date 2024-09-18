9f26441d933413dfaf8ed9371d9f182a84c87f10
CoralPink <03_stylist_triages@icloud.com>
2024-09-18 17:17:44 +0900

    Do not load `searchindex.json` unless you use search 🌾 (#198)
    * Do not load `searchindex.json` unless you use search 🌾
    
    * Minor update to cache version 🌕

--- js/book.js
+++ js/book.js
@@ -1,5 +1,5 @@
 import { procCodeBlock } from './codeblock.js';
-import { initGlobalSearch, initSearch } from './searcher.js';
+import { initGlobalSearch, startUpSearch } from './searcher.js';
 import { initSidebar } from './sidebar.js';
 import { initTableOfContents } from './table-of-contents.js';
 import { initTheme, initThemeSelector } from './theme-selector.js';
@@ -7,27 +7,16 @@ import { initTheme, initThemeSelector } from './theme-selector.js';
 import initWasm, { attribute_external_links } from './wasm_book.js';
 
 const initialize = async () => {
+  initTableOfContents();
+  procCodeBlock();
+  initThemeSelector();
+
   const root = document.getElementById('bookjs').dataset.pathtoroot;
 
-  const [config, _] = await Promise.all([
-    fetch(`${root}searchindex.json`).then(response => response.json()),
-    initWasm({ module_or_path: `${root}wasm_book_bg.wasm` }),
-
-    initTableOfContents(),
-    procCodeBlock(),
-    initThemeSelector(),
-  ]);
-
-  attribute_external_links();
-  initSearch(root, config);
-
-  // Suppress "submit" events so the page doesn't reload when the user presses Enter
-  document.addEventListener('submit', e => e.preventDefault(), { once: false, passive: false });
-
-  // capture hover event in iOS
-  if (globalThis.ontouchstart !== undefined) {
-    document.addEventListener('touchstart', () => {}, { once: false, passive: true });
-  }
+  initWasm({ module_or_path: `${root}wasm_book_bg.wasm` }).then(() => {
+    attribute_external_links();
+    startUpSearch(root);
+  });
 };
 
 (() => {
@@ -36,4 +25,12 @@ const initialize = async () => {
   initGlobalSearch();
 
   document.addEventListener('DOMContentLoaded', initialize, { once: true, passive: true });
+
+  // Suppress "submit" events so the page doesn't reload when the user presses Enter
+  document.addEventListener('submit', e => e.preventDefault(), { once: false, passive: false });
+
+  // capture hover event in iOS
+  if (globalThis.ontouchstart !== undefined) {
+    document.addEventListener('touchstart', () => {}, { once: false, passive: true });
+  }
 })();
