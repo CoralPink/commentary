3c8294fa1a7a11521585245fda0f7e99f1375b63
CoralPink <03_stylist_triages@icloud.com>
2024-12-06 14:40:47 +0900

    Since `<form>` no longer exists, there is no need to operate on the `submit` event.

--- js/book.js
+++ js/book.js
@@ -28,9 +28,6 @@ const initialize = () => {
 
   document.addEventListener('DOMContentLoaded', initialize, { once: true, passive: true });
 
-  // Suppress "submit" events so the page doesn't reload when the user presses Enter
-  document.addEventListener('submit', e => e.preventDefault(), { once: false, passive: false });
-
   // capture hover event in iOS
   if (globalThis.ontouchstart !== undefined) {
     document.addEventListener('touchstart', () => {}, { once: false, passive: true });
