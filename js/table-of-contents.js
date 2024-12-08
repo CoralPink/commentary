0f7ae34028f173ada1b799bbba6d865a10e2eb48
CoralPink <03_stylist_triages@icloud.com>
2024-12-08 16:27:26 +0900

    Improve `toc` in mobile environments 🛩️
    It looks like scrollIntoView is doing something bad, but nothing is clear...!

--- js/table-of-contents.js
+++ js/table-of-contents.js
@@ -6,7 +6,6 @@ const ENV_MOBILE = 1;
 const ELEMENT_TOC = ['righttoc', 'bottomtoc'];
 
 const mobileMaxWidth = getRootVariableNum('--mobile-max-width');
-const isChromium = !!window.chrome;
 
 const tocMap = new Map();
 let observer;
@@ -14,27 +13,27 @@ let observer;
 let environment;
 let onlyActive = null;
 
+let currentInlineCenter = null;
+
+const scrollCenter = () => {
+  if (environment === ENV_PC || currentInlineCenter === null) {
+    return;
+  }
+  setTimeout(() => {
+    currentInlineCenter.scrollIntoView({ inline: 'center' });
+  });
+};
+
 const addActive = entry => {
   if (onlyActive !== null) {
     onlyActive.classList.remove('active');
     onlyActive = null;
   }
-  const active = tocMap.get(entry.target);
-  active.classList.add('active');
-  active.setAttribute('aria-current', 'true');
+  const target = tocMap.get(entry.target);
+  target.classList.add('active');
+  target.setAttribute('aria-current', 'true');
 
-  if (environment === ENV_PC) {
-    return;
-  }
-
-  // TODO: Only chromium operates differently, so the process is temporarily divided and dealt with as an emergency measure.
-  //       However, even this is not sufficient...
-  if (isChromium) {
-    active.addEventListener('scrollend', ev => ev.scrollIntoView({ inline: 'center' }), { once: true, passive: true });
-    return;
-  }
-
-  active.scrollIntoView({ inline: 'center' });
+  currentInlineCenter = target;
 };
 
 const removeActive = entry => {
@@ -57,12 +56,19 @@ const removeActive = entry => {
   target.removeAttribute('aria-current');
 };
 
+function* reverseItr(array) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    yield array[i];
+  }
+}
+
 const initialize = () => {
   observer = new IntersectionObserver(
     entries => {
-      for (const x of entries) {
+      for (const x of reverseItr(entries)) {
         x.isIntersecting ? addActive(x) : removeActive(x);
       }
+      scrollCenter();
     },
     { threshold: 1.0 },
   );
