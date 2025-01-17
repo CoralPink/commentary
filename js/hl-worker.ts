93a260854781060202d04085541f7f9b9965e021
CoralPink <03_stylist_triages@icloud.com>
2025-01-17 13:39:03 +0900

    参ノ型

--- js/hl-worker.ts
+++ js/hl-worker.ts
@@ -1,15 +1,22 @@
-import { highlight } from './highlight.js/build/highlight.js';
+import hljs from './highlight.js/build/highlight.js';
+
+const DELETEING_PREFIX = 'language-'.length;
 
 const NERD_FONT_RANGE = /[\uE000-\uF8FF]/;
-const containsNerdFontIcon = text => NERD_FONT_RANGE.test(text);
+const containsNerdFontIcon = (text: string) => NERD_FONT_RANGE.test(text);
 
-self.onmessage = ev => {
-  const highlightCode = highlight(ev.data[0], {
-    language: ev.data[1].slice(9), // Remove the string "language-" before passing.
+interface PostMessageData {
+  highlightCode: string;
+  needNerdFonts: boolean;
+}
+
+self.onmessage = (ev: MessageEvent<string[]>): void => {
+  const highlightCode = hljs.highlight(ev.data[0], {
+    language: ev.data[1].slice(DELETEING_PREFIX),
     ignoreIllegals: true,
   }).value;
 
   const needNerdFonts = containsNerdFontIcon(ev.data[0]);
 
-  postMessage({ highlightCode, needNerdFonts });
+  postMessage({ highlightCode, needNerdFonts } as PostMessageData);
 };
