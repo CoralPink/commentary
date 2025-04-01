e8b0656528a90ed56e63e76fabb277d302e32231
CoralPink <03_stylist_triages@icloud.com>
2025-04-01 14:55:47 +0900

    Action to update `mdbook` to v0.4.48 📖 (#398)
    * Action to update `mdbook` to v0.4.48 📖
    
    * Temporarily enable `build-and-deploy`.
    
    * finishing touches.

--- debug.sh
+++ debug.sh
@@ -48,6 +48,9 @@ sed \
   -e 's|<li class="chapter-item expanded ">|<li>|g' \
   -e 's| target="_parent"||g' \
   toc.html > pagelist.html
+
+sed -E 's/^Object\.assign\(window\.search, //; s/\);$//' searchindex.js > searchindex.json
+jq empty searchindex.json && printf '    \e[1;32mFinished\e[0m convert search index \e[33m🧶Did it!!\e[0m\n'
 popd
 
 #pushd commentary
