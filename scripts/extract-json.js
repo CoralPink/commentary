0bbb41bfbb7813a38f0829c3740a5bf11c52daee
CoralPink <03_stylist_triages@icloud.com>
2025-05-06 16:41:12 +0900

    Adding error handling for file operations

--- scripts/extract-json.js
+++ scripts/extract-json.js
@@ -1,14 +1,33 @@
 import fs from 'node:fs';
 
-const code = fs.readFileSync('searchindex.js', 'utf8');
+const FILE_INDEX = 'searchindex';
+
+const jsName = `${FILE_INDEX}.js`;
+const jsonName = `${FILE_INDEX}.json`;
+
 const sandbox = {};
+
+const extract = () => {
+  try {
+    return fs.readFileSync(`${jsName}`, 'utf8');
+  } catch (error) {
+    console.error(`Error reading ${jsName}`, error.message);
+    process.exit(1);
+  }
+};
 
 // biome-ignore lint/security/noGlobalEval: Using eval in a controlled sandbox to execute the JS from searchindex.js
 eval(`
   const window = {};
-  ${code}
+  ${extract()}
   sandbox.result = window.search;
 `);
 
-fs.writeFileSync('searchindex.json', JSON.stringify(sandbox.result, null, 2));
-console.log('✅ complete: JS -> JSON');
+try {
+  fs.writeFileSync(jsonName, JSON.stringify(sandbox.result, null, 2));
+
+  console.info(`✅ complete: ${jsonName}`);
+} catch (error) {
+  console.error(`Error writing ${jsonName}`, error.message);
+  process.exit(1);
+}
