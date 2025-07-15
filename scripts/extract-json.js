731553b7db5e75117ae1fd847947f27f945baad1
CoralPink <03_stylist_triages@icloud.com>
2025-07-15 15:53:31 +0900

    Mysterious work involving `mdbook` updates 🕵️‍♀️
    Well, that's because I'm changing it without permission and using it.

--- scripts/extract-json.js
+++ scripts/extract-json.js
@@ -1,4 +1,4 @@
-import fs from 'node:fs';
+import { readFileSync, writeFileSync } from 'node:fs';
 
 const FILE_INDEX = 'searchindex';
 
@@ -9,7 +9,7 @@ const FILE_INDEX = 'searchindex';
     const jsFile = `${FILE_INDEX}.js`;
 
     try {
-      return fs.readFileSync(`${jsFile}`, 'utf8');
+      return readFileSync(`${jsFile}`, 'utf8');
     } catch (error) {
       console.error(`Error reading ${jsFile}`, error.message);
       process.exit(1);
@@ -19,14 +19,17 @@ const FILE_INDEX = 'searchindex';
   // biome-ignore lint/security/noGlobalEval: Using eval in a controlled sandbox to execute the JS from searchindex.js
   eval(`
     const window = {};
+    window.search = {};
+
     ${extract()}
+
     sandbox.result = window.search;
   `);
 
   const jsonFile = `${FILE_INDEX}.json`;
 
   try {
-    fs.writeFileSync(jsonFile, JSON.stringify(sandbox.result));
+    writeFileSync(jsonFile, JSON.stringify(sandbox.result));
     console.info('    \x1b[1;32mFinished\x1b[0m convert search index \x1b[33m🧶Did it!!\x1b[0m');
   } catch (error) {
     console.error(`Error writing ${jsonFile}`, error.message);
