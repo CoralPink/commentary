0306fc16a3a405d05dcd5980cdbd776b70528e43
CoralPink <03_stylist_triages@icloud.com>
2025-10-22 12:50:59 +0900

    Consider using globalThis.crypto for consistency. / Bind `randomUUID`

--- js/hl-initialize.ts
+++ js/hl-initialize.ts
@@ -11,16 +11,21 @@ const WORKER_PATH = '/commentary/hl-worker.js';
 
 type WorkerCallback = (data: Payload) => void;
 
-const randomId = (): string => {
+const fallbackRandomHexId = (): string => {
   const array = new Uint8Array(16);
-  crypto.getRandomValues(array);
+  globalThis.crypto.getRandomValues(array);
 
   return Array.from(array)
     .map(b => b.toString(16).padStart(2, '0'))
     .join('');
 };
 
-const getRandomId = (): string => (typeof self.crypto?.randomUUID === 'function' ? self.crypto.randomUUID : randomId)();
+const randomUUID: () => string =
+  typeof globalThis.crypto === 'object' && typeof globalThis.crypto.randomUUID === 'function'
+    ? globalThis.crypto.randomUUID.bind(globalThis.crypto)
+    : fallbackRandomHexId;
+
+const getRandomId = (): string => randomUUID();
 
 /**
  * Initializes a SharedWorker-based communication system.
