abca69f6ad7b11d287a5e848455c182f025f0d89
CoralPink <03_stylist_triages@icloud.com>
2025-10-22 11:55:52 +0900

    Add error handling for directory operations.

--- js/create-highlight.sh
+++ js/create-highlight.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/highlightjs/highlight.js.git -b 11.11.0 --depth 1 highlight
-pushd highlight
+pushd highlight || exit 1
 
 npm install
 node tools/build.js -n bash diff json lua vim
 
-popd
+popd || exit 1
