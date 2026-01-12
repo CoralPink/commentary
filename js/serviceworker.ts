55a57fff723a9cecc091b33ae23142013c790206
CoralPink <03_stylist_triages@icloud.com>
2026-01-12 23:12:55 +0900

    fix: The `footnote` pop-up has unintentionally acquired Violet Parr's ability 🦸‍♀️

--- js/serviceworker.ts
+++ js/serviceworker.ts
@@ -1,6 +1,6 @@
 declare const self: ServiceWorkerGlobalScope;
 
-const CACHE_VERSION = 'v10.2.0';
+const CACHE_VERSION = 'v10.2.1';
 
 const CACHE_URL = '/commentary/';
 const FALLBACK_IMAGE = 'favicon.png';
