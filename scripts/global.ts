1e58871699cfc4cf24688efb823fe3faac701147
CoralPink <03_stylist_triages@icloud.com>
2026-01-25 02:08:56 +0900

    Workflow Improvement (again) 🏂

--- scripts/global.ts
+++ scripts/global.ts
@@ -3,7 +3,6 @@ export const CLR_BC = '\x1b[1;35m';
 export const CLR_BG = '\x1b[1;32m';
 export const CLR_G = '\x1b[32m';
 
-export const PATH_DIRECTORY = '../commentary/';
 export const HTML_TOC = 'toc.html';
 
 const URL_PRODUCTION = 'https://coralpink.github.io/commentary/';
@@ -18,3 +17,5 @@ const rootFromArg = getArgValue('--root');
 
 export const isDebug = Deno.args.includes('--debug');
 export const rootPath = rootFromArg ?? (isDebug ? URL_LOCALHOST : URL_PRODUCTION);
+
+export const PATH_DIRECTORY = isDebug ? '../commentary/': '../book/';
