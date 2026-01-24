15a8c6f9daee9c171081f9beaf4543a451f0a072
CoralPink <03_stylist_triages@icloud.com>
2026-01-25 01:50:50 +0900

    I went and did something I shouldn't have...

--- js/link.ts
+++ js/link.ts
@@ -9,26 +9,36 @@ type LinkKind = (typeof LinkKind)[keyof typeof LinkKind];
 
 const isHttpProtocol = (s: URL): boolean => s.protocol === 'http:' || s.protocol === 'https:';
 
-const getLinkKind = (elm: HTMLAnchorElement): LinkKind => {
+const hrefToURL = (href: string): URL | null => {
+  try {
+    return new URL(href, globalThis.location.href);
+  } catch {
+    // mailto:, tel:, invalid url
+    return null;
+  }
+};
+
+export const getLinkKind = (elm: HTMLAnchorElement): LinkKind => {
   const rawHref = elm.getAttribute('href');
 
   if (!rawHref) {
     console.error('link: not found href');
     return LinkKind.Undefined;
   }
+
   if (rawHref.startsWith('#')) {
     return LinkKind.Native;
   }
-  if (rawHref.startsWith('.') || rawHref.startsWith('/')) {
-    return LinkKind.Internal;
-  }
 
-  try {
-    const url = new URL(rawHref);
-    return isHttpProtocol(url) ? LinkKind.External : LinkKind.Native;
-  } catch {
-    return LinkKind.Undefined;
+  const url = hrefToURL(rawHref);
+
+  if (url === null || !isHttpProtocol(url)) {
+    return LinkKind.Native;
   }
+  if (url.origin === globalThis.location.origin) {
+    return url.pathname.endsWith('.html') ? LinkKind.Internal : LinkKind.External;
+  }
+  return LinkKind.External;
 };
 
 export const isExternalLink = (elm: HTMLAnchorElement): boolean => getLinkKind(elm) === LinkKind.External;
