b71e0ff815c1445f90915d3ec8e23c95c34f214d
CoralPink <03_stylist_triages@icloud.com>
2026-09-21 00:45:58 +0900

    I think it's best to use `disconnectedCallback()` to remove an `iframe`.

--- js/extensions/youtube.ts
+++ js/extensions/youtube.ts
@@ -28,8 +28,11 @@ class YouTubeVideo extends HTMLElement {
     iframe.src = `${SRC_URL}${id}`;
     iframe.allow = 'fullscreen';
 
-    this.shadowRoot!.querySelectorAll('iframe').forEach((element) => element.remove());
     this.shadowRoot!.append(iframe);
+  }
+
+  disconnectedCallback(): void {
+    this.shadowRoot!.querySelector('iframe')?.remove();
   }
 }
 
